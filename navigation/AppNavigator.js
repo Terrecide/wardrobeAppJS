e283a906a40b7be9bd4d5df3259e15730589d988
alex.pantaleev.98@gmail.com <alex.pantaleev.98@gmail.com>
2021-11-02 17:19:36 +0200

    plus * 2

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -19,7 +19,7 @@ function HomeTabs() {
       <Tab.Navigator screenOptions={{tabBarShowLabel: false}}>
         <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="home-outline" size={size} color={color} />}} name="Home" component={HomeScreen} />
         <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="search-outline" size={size} color={color} />}} name="Search" component={SearchScreen} />
-        <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="add-circle-outline" size={size} color={color} />}} name="AddListing" component={AddListingScreen} />
+        <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="add-circle" size={size*2} color={color} />}} name="AddListing" component={AddListingScreen} />
         <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="mail-outline" size={size} color={color} />}} name="Notifications" component={NotificationsScreen} />
         <Tab.Screen options={{tabBarIcon: ({size, color}) => <Ionicons name="person-outline" size={size} color={color} />}} name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
